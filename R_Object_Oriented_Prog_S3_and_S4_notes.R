e1dd5257379abab8ebad3b778042f3e3c317ab47
allanbeto <alan.alvarez@ciencias.unam.mx>
2017-08-06 13:48:11 -0500

    sync repository

---
+++ R_Object_Oriented_Prog_S3_and_S4_notes.R
@@ -0,0 +1,26 @@
+# Course Description Object-oriented programming (OOP) lets you specify
+# relationships between functions and the objects that they can act on, helping
+# you manage complexity in your code. This is an intermediate level course,
+# providing an introduction to OOP, using the S3 and R6 systems. S3 is a great
+# day-to-day R programming tool that simplifies some of the functions that you
+# write. R6 is especially useful for industry-specific analyses, working with
+# web APIs, and building GUIs. The course concludes with an interview with
+# Winston Chang, creator of the R6 package.
+# 
+# Introduction to Object-Oriented Programming Learn what object-oriented
+# programming (OOP) consists of, when to use it, and what OOP systems are
+# available in R. You'll also learn how R identifies different types of
+# variable, using classes, types, and modes.
+# 
+# Using S3S3 is a very simple object-oriented system that lets you define
+# different behavior for functions, depending upon their input argument. This
+# chapter explains how to use S3, and how generics and methods work.Using
+# R6Learn how to define R6 classes, and to create R6 objects. You'll also learn
+# about the structure of R6 classes, and how to separate the user interface from
+# the implementation details. Using R6Learn how to define R6 classes, and to
+# create R6 objects. You'll also learn about the structure of R6 classes, and
+# how to separate the user interface from the implementation details. Advanced
+# R6 UsageComplete your mastery of R6 by learning about advanced topics such as
+# copying by reference, shared fields, cloning objects, and finalizing objects.
+# The chapter concludes with an interview with Winston Chang, creator of the R6
+# package.
