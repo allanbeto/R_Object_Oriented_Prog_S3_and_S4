70162e59e8e1b72a398e251dc0a769235e4f2195
allanbeto <alan.alvarez@ciencias.unam.mx>
2017-08-13 21:32:24 -0500

    last chapter

--- R_Object_Oriented_Prog_S3_and_S4_notes.R
+++ R_Object_Oriented_Prog_S3_and_S4_notes.R
@@ -13,12 +13,15 @@
 
 install.packages("pryr")
 install.packages("R6")
-
+install.packages("assertive.types")
+install.packages("assertive.numbers")
 
 #Library
+library(pryr)
+library(R6)
+library(assertive.types)
+library(assertive.numbers)
 
-library("pryr")
-library("R6")
 #-------------------------------------------------------------> Introduction to Object-Oriented Programming
 
 # Learn what object-oriented programming (OOP) consists of, when to use it, and
@@ -106,13 +109,6 @@ is.list(chess)
 # How many pieces are left on the board?
 length(unlist(chess))
 
-
-
-
-
-
-
-
 #-------------------------------------------------------------> Using S3
 
 # S3is a very simple object-oriented system that lets you define different
@@ -151,7 +147,7 @@ n_elements_sleep
 #--------------------------------------> Creating an S3 method (2)
 
 # View pre-defined objects
-ls.str()
+#ls.str()
 
 # Create a default method for get_n_elements
 get_n_elements.default <- function(x,...){
@@ -262,25 +258,420 @@ microwave_oven_factory
 # Make a new microwave oven
 microwave_oven <- microwave_oven_factory$new() 
 
+#--------------------------------------> Learning to Cook
+
+# Add a cook method to the factory definition
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    power_rating_watts = 800
+  ),
+  public = list(
+    cook = function(time_seconds){
+      Sys.sleep(time_seconds)
+      print("Your food is cooked!")
+    }
+  )
+)
+
+# Create microwave oven object
+a_microwave_oven <- microwave_oven_factory$new()
+
+# Call cook method for 1 second
+a_microwave_oven$cook(1)
+
+#--------------------------------------> Close the Door
+
+# Add a close_door() method
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    power_rating_watts = 800,
+    door_is_open = FALSE
+  ),
+  public = list(
+    cook = function(time_seconds) {
+      Sys.sleep(time_seconds)
+      print("Your food is cooked!")
+    },
+    open_door = function() {
+      private$door_is_open = TRUE
+    },
+    close_door = function() {
+      private$door_is_open = FALSE
+    }
+    
+    
+  )
+)
+
+#-------------------------------------->First Thing's First
+
+# Add an initialize method
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    power_rating_watts = 800,
+    door_is_open = FALSE
+  ),
+  public = list(
+    cook = function(time_seconds) {
+      Sys.sleep(time_seconds)
+      print("Your food is cooked!")
+    },
+    open_door = function() {
+      private$door_is_open = TRUE
+    },
+    close_door = function() {
+      private$door_is_open = FALSE
+    },
+    # Add initialize() method here
+    initialize = function(power_rating_watts, door_is_open) {
+      if (!missing(power_rating_watts)) {
+        private$power_rating_watts <- power_rating_watts
+      }
+      if (!missing(door_is_open)) {
+        private$door_is_open <- door_is_open
+      }
+    }
+    
+    
+  )
+)
+
+# Make a microwave
+a_microwave_oven <- microwave_oven_factory$new(
+  power_rating_watts = 650,
+  door_is_open = TRUE
+)
+
+
+#--------------------------------------> Read the Rating
+
+# Add a binding for power rating
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    ..power_rating_watts = 800
+  ),
+  active = list(
+    # add the binding here
+    power_rating_watts = function() {
+      private$..power_rating_watts
+    }
+    
+  )
+)
+
+# Make a microwave 
+a_microwave_oven <- microwave_oven_factory$new() 
+
+# Get the power rating
+
+a_microwave_oven$power_rating_watts
+
+
+#--------------------------------------> Control the Power
+
+# Add a binding for power rating
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    ..power_rating_watts = 800,
+    ..power_level_watts = 800
+  ),
+  # Add active list containing an active binding
+  active = list(
+    power_level_watts = function(value) {
+      if (missing(value)) {
+        private$..power_level_watts
+      }
+      else {
+        assert_is_a_number(value)
+        assert_all_are_in_closed_range(value, 0, private$..power_rating_watts)
+        private$..power_level_watts <- value
+      }
+    }
+  )
+)
+# Make a microwave 
+a_microwave_oven <- microwave_oven_factory$new()
+
+# Get the power level
+a_microwave_oven$power_level_watts 
+
+# Try to set the power level to "400"
+a_microwave_oven$power_level_watts <- "400"
+
+# Try to set the power level to 1600 watts
+a_microwave_oven$power_level_watts <- 1600
+
+# Set the power level to 400 watts
+a_microwave_oven$power_level_watts <- 400
 
 
 
-#-------------------------------------->
-#-------------------------------------->
-#-------------------------------------->
-#-------------------------------------->
 #-------------------------------------------------------------> Using R6Learn how to define R6
+
 
 # classes, and to create R6 objects. You'll also learn about the structure of R6
 # classes, and how to separate the user interface from the implementation
 # details. 
 
-#-------------------------------------->
-#-------------------------------------->
-#-------------------------------------->
-#-------------------------------------->
-#-------------------------------------->
-#-------------------------------------->
+#--------------------------------------> Specifying a Fancy Microwave Oven
+
+# Explore the microwave oven class
+microwave_oven_factory
+
+# Define a fancy microwave class inheriting from microwave oven
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory
+) 
+
+
+#--------------------------------------> Making a Fancy Microwave Oven
+
+#PRECODE
+library(R6)
+library(assertive.types)
+library(assertive.numbers)
+
+microwave_oven_factory <- R6Class("MicrowaveOven",
+                                  private = list(
+                                    ..power_rating_watts = 800,
+                                    ..power_level_watts = 800,
+                                    ..door_is_open = FALSE
+                                  ),
+                                  public = list(
+                                    cook = function(time_seconds) {
+                                      Sys.sleep(time_seconds)
+                                      print("Your food is cooked!")
+                                    },
+                                    open_door = function() {
+                                      private$..door_is_open = TRUE
+                                    },
+                                    close_door = function() {
+                                      private$..door_is_open = FALSE
+                                    }
+                                  ),
+                                  active = list(
+                                    power_rating_watts = function() {
+                                      private$..power_rating_watts
+                                    },
+                                    power_level_watts = function(value) {
+                                      if (missing(value)) {
+                                        private$..power_level_watts
+                                      } else {
+                                        assert_is_a_number(value)
+                                        assert_all_are_in_closed_range(
+                                          value, 0, private$..power_rating_watts
+                                        )
+                                        private$..power_level_watts <- value
+                                      }
+                                    }
+                                  )
+)                                  
+
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  public = list(
+    cook_baked_potato = function() {
+      self$cook(3)
+    },
+    cook = function(time_seconds) {
+      super$cook(time_seconds)
+      message("Enjoy your dinner!")
+    }
+  ),
+  active = list(
+    super_ = function() super
+  )
+)
+
+# -----------------------------------------
+                        
+                                                
+# Explore microwave oven classes
+microwave_oven_factory
+fancy_microwave_oven_factory
+
+# Instantiate both types of microwave
+a_microwave_oven <- microwave_oven_factory$new()
+a_fancy_microwave <- fancy_microwave_oven_factory$new()
+
+# Get power rating for each microwave
+microwave_power_rating <- a_microwave_oven$power_rating_watts 
+fancy_microwave_power_rating <- a_fancy_microwave$power_rating_watts
+
+# Verify that these are the same
+identical(microwave_power_rating, a_fancy_microwave$power_rating_watts)
+
+# Cook with each microwave
+a_microwave_oven$cook(1)
+a_fancy_microwave$cook(1)
+
+#--------------------------------------> Extending the Cooking Capabilities
+
+# Explore microwave oven class
+microwave_oven_factory
+
+# Extend the class definition
+fancy_microwave_oven_factory <- R6Class(  
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  # Add a public list with a cook baked potato method
+  public = list(
+    cook_baked_potato = function() {
+      self$cook(3)
+    }
+  )
+)
+# Instantiate a fancy microwave
+a_fancy_microwave <- fancy_microwave_oven_factory$new()
+# Call the cook_baked_potato() method
+a_fancy_microwave$cook_baked_potato()
+
+#--------------------------------------> Overriding the Cooking Capabilities
+
+# Explore microwave oven class
+microwave_oven_factory
+
+# Update the class definition
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  # Add a public list with a cook method
+  public = list(
+    cook = function(time_seconds) {
+      super$cook(time_seconds)
+      message("Enjoy your dinner!")
+    }
+  )
+)  
+# Instantiate a fancy microwave
+a_fancy_microwave <- fancy_microwave_oven_factory$new() 
+
+# Call the cook() method
+a_fancy_microwave$cook(1)
+
+
+#--------------------------------------> Exposing your Parent
+
+# Expose the parent functionality
+fancy_microwave_oven_factory <- R6Class(
+  "FancyMicrowaveOven",
+  inherit = microwave_oven_factory,
+  public = list(
+    cook_baked_potato = function() {
+      self$cook(3)
+    },
+    cook = function(time_seconds) {
+      super$cook(time_seconds)
+      message("Enjoy your dinner!")
+    }
+  ),
+  # Add an active element with a super_ binding
+  active = list(
+    super_ = function() super
+  )
+)
+
+# Instantiate a fancy microwave
+a_fancy_microwave <- fancy_microwave_oven_factory$new() 
+
+# Call the super_ binding
+a_fancy_microwave$super_
+
+#--------------------------------------> Over-Overriding the Cooking Capabilities
+library(R6)
+library(assertive.types)
+library(assertive.numbers)
+microwave_oven_factory <- R6Class("MicrowaveOven",
+                                  private = list(
+                                    ..power_rating_watts = 800,
+                                    ..power_level_watts = 800,
+                                    ..door_is_open = FALSE
+                                    ),
+                                  public = list(
+                                    cook = function(time_seconds) {
+                                      Sys.sleep(time_seconds)
+                                      print("Your food is cooked!")
+                                      },
+                                    open_door = function() {
+                                      private$..door_is_open = TRUE
+                                      },
+                                    close_door = function() {
+                                      private$..door_is_open = FALSE
+                                      }
+                                    ),
+                                  active = list(
+                                    power_rating_watts = function() {
+                                      private$..power_rating_watts
+                                      },
+                                    power_level_watts = function(value) {
+                                      if (missing(value)) {
+                                        private$..power_level_watts
+                                        } else {
+                                          assert_is_a_number(value)
+                                          assert_all_are_in_closed_range(
+                                            value, 0, private$..power_rating_watts
+                                            )
+                                          private$..power_level_watts <- value
+                                          }
+                                      }
+                                    )
+)                                  
+
+fancy_microwave_oven_factory <- R6Class(
+                                   "FancyMicrowaveOven",
+                                   inherit = microwave_oven_factory,
+                                   public = list(
+                                       cook_baked_potato = function() {
+                                       self$cook(3)
+                                     },
+                                       cook = function(time_seconds) {
+                                       super$cook(time_seconds)
+                                       message("Enjoy your dinner!")
+                                     }
+                                           ),
+                                    active = list(
+                                       super_ = function() super
+                                     )
+                              )
+
+
+ascii_pizza_slice <- "   __
+\n // \"\"--.._
+\n||  (_)  _ \"-._
+\n||    _ (_)    '-.
+\n||   (_)   __..-'
+\n \\\\__..--\"\""
+
+
+
+# Explore other microwaves
+microwave_oven_factory
+fancy_microwave_oven_factory
+
+# Define a high-end microwave oven class
+high_end_microwave_oven_factory <- R6Class(
+  "HighEndMicrowaveOven",
+  inherit = fancy_microwave_oven_factory,
+  public = list(
+    cook = function(time_seconds) {
+      super$super_$cook(time_seconds)  
+      message(ascii_pizza_slice)
+    }
+  )
+)
+
+# Instantiate a high-end microwave oven
+a_high_end_microwave <- high_end_microwave_oven_factory$new()
+
+# Use it to cook for one second
+a_high_end_microwave$cook(1)
 
 
 #-------------------------------------------------------------> Advanced R6
